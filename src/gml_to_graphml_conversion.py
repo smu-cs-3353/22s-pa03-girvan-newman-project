1576b1ed4e64969ebdbfe3688696e66e041d29de
Mikecamdo <mcdoherty@smu.edu>
2022-04-07 23:05:35 -0500

    Beginning to implement Label Propagation
    Implemented a very primitive form of the Label Propagation Algorithm (it currently does not go in a random order, it does not break ties randomly, and it only loops through the nodes once).

---
+++ src/gml_to_graphml_conversion.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def start():
+    #H = nx.read_gml("Users\mikec_g1kgiu8\OneDrive\Desktop\CS 3353 Projects\22s-pa03-girvan-newman-a-loner-s-regret\RandomGraphs\test.gml")
+    
+    #f = open('Users\mikec_g1kgiu8\OneDrive\Desktop\CS 3353 Projects\22s-pa03-girvan-newman-a-loner-s-regret\RandomGraphs\test.graphml', 'r')
+    f = open("../RandomGraphs/test.graphml")
+
+    #G = nx.write_graphml_lxml(H, "test.graphml")
+    #X = nx.read_graphml('test.graphml')
+    X = nx.read_graphml("../RandomGraphs/test.graphml")
+    nx.draw(X)
+    plt.show()
+
+if __name__ == '__main__':
+    start()
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 // Created by mikec_g1kgiu8 on 4/4/2022.
 //
 #include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_map>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graphml.hpp>
 #include <boost/graph/breadth_first_search.hpp>
-#include <boost/parameter/parameters.hpp>
-#include <boost/pending/indirect_cmp.hpp>
 #include <boost/range/irange.hpp>
 
-using namespace std;
+//using namespace std;
 using namespace boost;
 
 int main() {
@@ -19,5 +20,53 @@ int main() {
     Graph g;
     Dproperty dp;
 
-    
+    std::ifstream graphFile ("../RandomGraphs/test.graphml");
+
+    read_graphml(graphFile, g, dp);
+
+    std::map<int, int> node_labels; //holds the current label for each node
+    std::map<int, std::vector<int>> neighbor_nodes; //holds the labels of the current node's neighbors
+
+    // Printing edges
+    for (auto v : boost::make_iterator_range(vertices(g))) {
+        std::cout << "Node " << v << std::endl;
+        node_labels[v] = v; //Initializing each node with its own unique label
+
+        //the following code was found at: https://stackoverflow.com/questions/49898415/boost-library-how-to-get-neighbouring-nodes
+        auto neighbors = boost::adjacent_vertices(v, g);
+        for (auto vd : make_iterator_range(neighbors)) {
+            std::cout << v << " has adjacent vertex " << vd << std::endl;
+            neighbor_nodes[v].push_back(vd); //adds each neighboring node to the vector
+        }
+
+//        for (auto oe : make_iterator_range(out_edges(v, g))){
+//            std::cout << "Edge " << oe << std::endl;
+//        }
+    }
+
+    for (auto v : boost::make_iterator_range(vertices(g))) { //FIXME this needs to be done in a random order
+        std::cout << "Currently on node " << v << std::endl;
+        std::unordered_map<int, int> label_frequency;
+
+        int new_label = -1;
+        int new_label_frequency = 0;
+
+        for (int i = 0; i < neighbor_nodes[v].size(); i++) {
+            label_frequency[node_labels[neighbor_nodes[v].at(i)]] += 1;
+
+            std::cout << label_frequency[node_labels[neighbor_nodes[v].at(i)]] << std::endl;
+            if (label_frequency[node_labels[neighbor_nodes[v].at(i)]] > new_label_frequency) { //FIXME if there are ties, it needs to be broken randomly
+                new_label = node_labels[neighbor_nodes[v].at(i)];
+            }
+        }
+
+        node_labels[v] = new_label;
+
+    }
+
+    for (auto v : boost::make_iterator_range(vertices(g))) {
+        std::cout << "Node " << v << "'s label is " << node_labels[v] << std::endl;
+    }
+
+    //cout << node_labels[1] << endl;
 }
