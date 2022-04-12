95140df837791202cee7f0e222d440587f5eba59
Mikecamdo <mcdoherty@smu.edu>
2022-04-11 21:43:03 -0500

    Bug Fix on when to stop Label Propagation
    Fixed a bug that caused Label Propagation to never end sometimes. Now the algorithm stops when every node has the most frequently appearing label among its neighbor nodes (before it stopped only if its label appeared on at least half of its neighbor nodes)

--- src/LabelPropagationAlgo.cpp
+++ src/LabelPropagationAlgo.cpp
@@ -57,7 +57,7 @@ void LabelPropagationAlgo::runAlgo() {
         std::cout << std::endl;
 
         for (int v : nodes) { //loops through each node (which are randomly sorted)
-            std::cout << "Currently on node " << v << std::endl;
+            std::cout << "Currently on node " << v << std::endl; //FIXME
             std::unordered_map<int, int> label_frequency;
 
             int new_label = -1;
@@ -68,7 +68,7 @@ void LabelPropagationAlgo::runAlgo() {
             for (int i = 0; i < neighbor_nodes[v].size(); i++) {
                 label_frequency[node_labels[neighbor_nodes[v].at(i)]] += 1;
 
-                std::cout << label_frequency[node_labels[neighbor_nodes[v].at(i)]] << std::endl;
+                std::cout << label_frequency[node_labels[neighbor_nodes[v].at(i)]] << std::endl; //FIXME
                 if (label_frequency[node_labels[neighbor_nodes[v].at(i)]] > new_label_frequency) {
                     new_label = node_labels[neighbor_nodes[v].at(i)];
                     new_label_frequency = label_frequency[node_labels[neighbor_nodes[v].at(i)]]; //updates the label frequency
@@ -82,39 +82,49 @@ void LabelPropagationAlgo::runAlgo() {
                 }
             }
 
+            std::cout << "Old label: " << node_labels[v] << std::endl;
             if (!new_label_options.empty()) { //if multiple neighbor node labels appear the same number of times
                 std::shuffle(new_label_options.begin(), new_label_options.end(), std::mt19937(std::random_device()())); //randomly shuffles the options
                 node_labels[v] = new_label_options.at(0);
             } else {
                 node_labels[v] = new_label;
             }
+            std::cout << "New label: " << node_labels[v] << std::endl;
 
         }
 
         for (auto v: boost::make_iterator_range(vertices(g))) {
             std::cout << "Node " << v << "'s label is " << node_labels[v] << std::endl;
-            //std::cout << "Node " << v << "'s data key is " << dp(v) << std::endl;
         }
 
         bool endAlgo = true; //variable that tells the label propagation to stop
 
         for (auto v: boost::make_iterator_range(vertices(g))) { //loops through every node
             int inCommon = 0; //counts how many neighbor nodes have the same label as the current node
+            //double threshold = neighbor_nodes[v].size() / 2.0; //label propagation can stop if the current node has the most frequent label among its neighbor nodes
+            std::unordered_map<int, int> label_frequency;
+            //int most_frequent = -1;
+            int most_frequent_frequency = 0;
 
             for (int i = 0; i < neighbor_nodes[v].size(); i++) {
                 if (node_labels[neighbor_nodes[v].at(i)] == node_labels[v]) { //if the current node and the neighbor node have the same label
                     inCommon++;
                 }
+
+                label_frequency[node_labels[neighbor_nodes[v].at(i)]] += 1;
+
+                if (label_frequency[node_labels[neighbor_nodes[v].at(i)]] > most_frequent_frequency) {
+                    //most_frequent = node_labels[neighbor_nodes[v].at(i)];
+                    most_frequent_frequency = label_frequency[node_labels[neighbor_nodes[v].at(i)]];
+                }
             }
 
-            double threshold = neighbor_nodes[v].size() / 2.0; //label propagation can stop if the current node has the same label as half (or more) of its neighbor nodes
-
-            if (inCommon < threshold) { //if the current node has a different label than most of its neighbors
+            if (inCommon < most_frequent_frequency) { //if the current node has a different label than the most frequent label among its neighbor nodes
+                std::cout << "FAILED ON " << v << std::endl;
                 endAlgo = false; //continue label propagation
                 break;
             }
         }
-        endAlgo = true; //FIXME temporarily stop after 1 iteration
 
         if (endAlgo) {
             break;
